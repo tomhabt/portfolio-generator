7756ace16453100a35869775139946f54ee233b9
thomas.habtemariam <thomas.habtemariam@gmail.com>
2021-07-19 23:05:41 -0400

    Page template completed

---
+++ try.js
@@ -0,0 +1,7 @@
+const animalArr = ['Dogs', 'Cats', 'Horses', 'Birds'];
+
+const coolAnimalArr = animalArr.map(animal => {
+  console.log(animal);
+  return `${animal} are really cool animals.`;
+});
+console.log(coolAnimalArr);
